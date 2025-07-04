e16a52b4ead16ae08f0308b9ea8ab0a093337234
Boi Nguyen <boi@devpanel.com>
2025-07-04 04:14:49 +0000

    install node 22

--- .devpanel/custom_package_installer.sh
+++ .devpanel/custom_package_installer.sh
@@ -12,14 +12,14 @@
 sudo apt-get update
 sudo apt-get install -y nano
 
-# Install VSCode Extensions
-if [[ -n "$DP_VSCODE_EXTENSIONS" ]]; then
-    sudo chown -R www:www $APP_ROOT/.vscode/extensions/
-    IFS=','
-    for value in $DP_VSCODE_EXTENSIONS; do
-        code-server --install-extension $value --user-data-dir=$APP_ROOT/.vscode
-    done
-fi
+echo '> Install node 22'
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+\. "$HOME/.nvm/nvm.sh"
+nvm install 22
+node -v # Should print "v22.17.0".
+nvm current # Should print "v22.17.0".
+npm -v # Should print "10.9.2".
+echo "> Installed Node $(node -v), NPM $(npm -v)"
 
 # Install Claude
 npm install @anthropic-ai/claude-code
@@ -28,3 +28,12 @@ sudo ln -s $(pwd)/node_modules/.bin/claude /usr/local/bin/claude
 # Install Google Gemini
 npm install @google/gemini-cli
 sudo ln -s $(pwd)/node_modules/.bin/gemini /usr/local/bin/gemini
+
+# Install VSCode Extensions
+if [[ -n "$DP_VSCODE_EXTENSIONS" ]]; then
+    sudo chown -R www:www $APP_ROOT/.vscode/extensions/
+    IFS=','
+    for value in $DP_VSCODE_EXTENSIONS; do
+        code-server --install-extension $value --user-data-dir=$APP_ROOT/.vscode
+    done
+fi
