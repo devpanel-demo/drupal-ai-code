2cd8b638df550e7223a9ac27fcf3a56bc88626ad
Boi Nguyen <boi@devpanel.com>
2025-07-04 07:15:13 +0000

    set os linux

--- .devpanel/custom_package_installer.sh
+++ .devpanel/custom_package_installer.sh
@@ -16,17 +16,14 @@ echo '> Install node 22'
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 \. "$HOME/.nvm/nvm.sh"
 nvm install 22
-node -v # Should print "v22.17.0".
-nvm current # Should print "v22.17.0".
-npm -v # Should print "10.9.2".
 echo "> Installed Node $(node -v), NPM $(npm -v)"
-
+npm config set os linux
 # Install Claude
-npm install @anthropic-ai/claude-code
+npm install @anthropic-ai/claude-code --force --no-os-check
 sudo ln -s $(pwd)/node_modules/.bin/claude /usr/local/bin/claude
 
 # Install Google Gemini
-npm install @google/gemini-cli
+npm install @google/gemini-cli --force --no-os-check
 sudo ln -s $(pwd)/node_modules/.bin/gemini /usr/local/bin/gemini
 
 # Install VSCode Extensions
